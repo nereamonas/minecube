da0f493bf490a0cb3188f0a0a7aa67046d740a6e
nereagce <48221213+nereagce@users.noreply.github.com>
2021-01-10 19:09:22 +0100

    Update conexion_bd.php

--- 3- Pagina web/html/conexion_bd.php
+++ 3- Pagina web/html/conexion_bd.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli("127.0.0.1", "root", "test", "myDb", 6033); //no se cual es user y cual bd
+$mysqli = new mysqli("127.0.0.1", "root", "test", "myDb", 6033); 
 if ($mysqli->connect_errno) {
   echo "Error al conectarse a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
